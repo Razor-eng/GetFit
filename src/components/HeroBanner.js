031f0ce63d04031dc29f23cfd3c877c89af72fa2
Razor-eng <83156526+Razor-eng@users.noreply.github.com>
2023-04-12 17:23:18 +0530

    first commit

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -5,7 +5,7 @@ import HeroBannerImage from '../assets/images/banner.png';
 
 const HeroBanner = () => (
   <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
-    <Typography color="#FF2625" fontWeight="600" fontSize="26px">Fitness Club</Typography>
+    <Typography color="#FF2625" fontWeight="600" fontSize="29px">GetFit</Typography>
     <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="23px" mt="30px">
       Sweat, Smile <br />
       And Repeat
